636222da2b1e7a79cc016e2f4f2c8c375f44e22d
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-12-19 12:35:31 -0500

    All budgets sanitized

--- app/service_objects/budget_service_object.rb
+++ app/service_objects/budget_service_object.rb
@@ -1,4 +1,21 @@
 class BudgetServiceObject
 
+  attr_accessor :unformatted_budget
+
+  def initialize(unformatted_budget)
+    @unformatted_budget = unformatted_budget
+  end
+
+  # NO BUDGET: 1929, 1937, 1944, 1949, 1952
+  # 1981 uses £
+  def budget
+    if unformatted_budget
+      budget_in_words = unformatted_budget.gsub(/ *\[[^)]*\] */, "").
+        gsub(/ *\([^)]*\) */, "").gsub("US","")
+      # convert_to_int()
+    else
+      "NO BUDGET NO BUDGET NO BUDGET NO BUDGET NO BUDGET NO BUDGET "
+    end
+  end
 
 end
